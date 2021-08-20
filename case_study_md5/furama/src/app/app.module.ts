3bdb9c9d6b316b92e02d5b77276414ddb58a142d
hyuganejidn@gmail.com <xuan hung>
2021-08-20 17:15:47 +0700

    task1-case

---
+++ case_study_md5/furama/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { CreateCustomerComponent } from './customer/create-customer/create-customer.component';
+import { MenuComponent } from './layout/menu/menu.component';
+import { CreateEmployeeComponent } from './employee/create-employee/create-employee.component';
+import { CreateServiceComponent } from './service/create-service/create-service.component';
+import { CreateContractComponent } from './contract/create-contract/create-contract.component';
+import { CreateContractDetailComponent } from './contract-detail/create-contract-detail/create-contract-detail.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CreateCustomerComponent,
+    MenuComponent,
+    CreateEmployeeComponent,
+    CreateServiceComponent,
+    CreateContractComponent,
+    CreateContractDetailComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
