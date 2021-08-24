0a7827458c4ef37ca96b0b59bd3b4004ec9440cd
hyuganejidn@gmail.com <xuan hung>
2021-08-24 21:34:04 +0700

    b6-bt2

---
+++ _6_angular_form/exercise/login/src/app/login/login.component.ts
@@ -0,0 +1,26 @@
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+ public loginForm = new FormGroup({
+    email: new FormControl('', [Validators.email, Validators.required]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)])
+  })
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit() {
+    console.log(this.loginForm.value);
+  }
+
+}
